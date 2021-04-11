f6f4cbedc7d36bba26fd42ceb1f40e11da55faeb
Gregory Bertrand <gregorybertrand@Gregorys-MacBook-Pro.local>
2021-04-11 12:57:25 -0500

    early afternoon

--- app.js
+++ app.js
@@ -28,7 +28,41 @@ document.querySelector('.sign-up-menu').addEventListener('submit', async (event)
         console.log(error)
     }
     
-    // document.querySelector('.sign-up').classList.add('hidden')
-    // document.querySelector('.login').classList.add('hidden')
-    // document.querySelector('#dictionary').classList.remove('hidden')
+})
+
+document.querySelector('.login-menu').addEventListener('submit', async (event) => {
+    event.preventDefault()
+
+    const emailInput = document.querySelector('#email').value
+    const passwordInput =document.querySelector('#password').value
+
+    document.querySelector('.sign-up').classList.add('hidden')
+    document.querySelector('.login').classList.add('hidden')
+    document.querySelector('#dictionary').classList.remove('hidden')
+
+    try {
+        const response = await axios.post('http://localhost:3001/user/login', {
+            email: emailInput,
+            password: passwordInput
+        })
+        console.log(response)
+
+        const userId = response.data.user.id
+        localStorage.setItem('userId', userId)
+        console.log(userId)
+
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+document.querySelector('.searchBar').addEventListener('submit', async (event) => {
+    event.preventDefault()
+    const searchResults = document.querySelector('.look-up').value
+    console.log(searchResults)
+
+    const userWord = document.querySelector('.word-container')
+    const choosenWord = document.createElement('h1')
+    choosenWord.textContent = searchResults
+    userWord.appendChild(choosenWord)
 })
