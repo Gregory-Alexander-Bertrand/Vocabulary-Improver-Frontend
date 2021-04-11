1c0c13f2ad9b87293daa9ab107b6c98dee47bcad
Gregory Bertrand <gregorybertrand@Gregorys-MacBook-Pro.local>
2021-04-11 10:51:16 -0500

    morning

--- app.js
+++ app.js
@@ -1,42 +1,8 @@
 
-// signUpMenu.addEventListener('submit', async (e) => {
-//     e.preventDefault()
-
-    // const nameInput = document.querySelector('#name').value 
-    // const emailInput = document.querySelector('#email').value
-    // const passwordInput = document.querySelector('#password').value
-    // console.log(nameInput, emailInput, passwordInput)
-//     try {
-//         const response = await axios.post('http://localhost:3001/user/create', {
-//             name: name,
-//             email: email,
-//             password: password
-//         })
-//         console.log(response)
-//         const userId = response.data.user.userId
-//         console.log(userId)
-//         localStorage.setItem('userId', userId)
-//         hideAndShow()
-//     } catch (error) {
-//         // console.log('login-failed')
-//     }
-
-//     // hideAndShow()
-// })
-
-// loginMenu.addEventListener('submit', (event) => {
-//     event.preventDefault()
-
-//     const emailInput = document.querySelector('#email').value
-//     const passwordInput = document.querySelector('#password').value
-//     console.log(emailInput, passwordInput)
-
-//     hideAndShow()
-// })
 
 document.querySelector('.sign-up-menu').addEventListener('submit', async (event) => {
     event.preventDefault()
-
+    console.log('button hit')
     const nameInput = document.querySelector('#name').value 
     const emailInput = document.querySelector('#email').value
     const passwordInput = document.querySelector('#password').value
@@ -49,13 +15,14 @@ document.querySelector('.sign-up-menu').addEventListener('submit', async (event)
 
     try {
         const response = await axios.post('http://localhost:3001/user/create', {
-            name: name,
-            email: email,
-            password: password
+            name: nameInput,
+            email: emailInput,
+            password: passwordInput
         })
         console.log(response)
 
         const userId = response.data.user.userId
+        console.log(userId)
         localStorage.setItem('userId', userId)
     } catch (error) {
         console.log(error)
