69d3250c00447395dde8eb539ee27e8635bf2488
Gregory Bertrand <gregorybertrand@Gregorys-MacBook-Pro.local>
2021-04-12 15:42:49 -0500

    switch backend url

--- server.js
+++ server.js
@@ -2,16 +2,26 @@ const express = require('express')
 const app = express()
 const routesReport = require('rowdy-logger').begin(app)
 const path = require('path')
+const replaceInFile = require('replace-in-file')
 
 app.get('/', (req, res) => {
     const filepath = path.join(__dirname, 'index.html')
     res.sendFile(filepath)
   })
   
-  app.get('/app.js', (req, res) => {
+  app.get('/app.js', async (req, res) => {
     const filepath = path.join(__dirname, 'app.js')
-    res.sendFile(filepath)
-  })
+
+    if (process.env.NODE_ENV === 'production') {
+    await replaceInFile({
+      files: filepath,
+      from: 'http://localhost:3001',
+      to: 'https://vocab-backend-app.herokuapp.com'
+    })
+   
+  }
+  res.sendFile(filepath)
+})
   
   app.get('/style.css', (req, res) => {
     const filepath = path.join(__dirname, 'style.css')
